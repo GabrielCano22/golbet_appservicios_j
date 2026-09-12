bbe76d7735e6aae4f9fe566f977c5333c67743fa
Gabriel Jaime Cano Oquendo <gabrieljaimecanooquendo@gmail.com>
2026-09-11 23:08:56 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,7 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using GolBet.Repositories.Data; 
+using Microsoft.EntityFrameworkCore;   
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);   
+
+builder.Services.AddControllersWithViews();  
+
+//Este es el nuevo código  
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 
